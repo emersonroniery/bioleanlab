5006ee24232f63d253745c90943b18d295e77968
emersonroniery <emersonroniery@gmail.com>
2025-12-08 10:23:14 -0300

    Feat: Install Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 // pages/_app.tsx
 
 import type { AppProps } from "next/app";
-
 import Script from "next/script";
-
+import { GoogleTagManager } from '@next/third-parties/google';
 import { useEffect, useState } from "react";
-
 import "../styles/globals.css";
-
-
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
@@ -97,6 +93,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
 
     <>
+      <GoogleTagManager gtmId="GTM-T6PG7DWP" />
 
       {/* Google AdSense Script - Estratégia adaptativa: mobile delay, desktop imediato */}
 
